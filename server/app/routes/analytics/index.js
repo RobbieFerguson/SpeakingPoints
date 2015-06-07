a1f22ac764832b45e12949ec72b1429fa475de9e
mbarzizza <mbarzizza@icloud.com>
2015-06-06 20:34:01 -0400

    literally nothing

--- server/app/routes/analytics/index.js
+++ server/app/routes/analytics/index.js
@@ -4,17 +4,23 @@ module.exports = router;
 var iod = require('iod-node')
 var client = new iod.IODClient('http://api.idolondemand.com', '3ada2b11-01fe-4e03-a23e-e0c9747013e7')
 var fs = require('fs');
+var path = require('path'); 
 
 
 router.get('/', function(req, res, next) {
 	console.log("Hitting analytics route");
-	client.call('recognizespeech', function(err, resData, body) {
-			// console.log("data", resData); 
-			console.log("body",  body);
-			res.status(200).send(JSON.stringify(body));
-	},data,true); 
+	var path = __dirname + '/test.wav'; 
+	console.log("path", path); 
+	var file = fs.readFileSync(path);
+	var data = {'file':path};
 
-	
+	client.call('recognizespeech',data,function(err,resp,body){
+		console.log("audio file sent");
+		console.log(err);
+		console.log(resp); 
+		console.log(body);
+		res.status(200).send("hitting route"); 
+	});
 })
 
 
